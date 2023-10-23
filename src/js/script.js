4a789e78342a3632f5b7ee662182a3afb759f26b
Gaetano <gaetanobarreca@gmail.com>
2023-10-22 19:13:17 -0700

    setting iframe source programmatically

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,14 @@ function customOpenForm() {
 function customCloseForm() {
   document.getElementById("customMyForm").style.display = "none";
 }
+
+async function setIframeSrc(){
+  const response = await fetch('https://asa-yt7xyaocl7xlo-demo.azuremicroservices.io/get-chatbot-token');
+  const json = await response.json();
+
+  debugger
+
+  document.getElementById("chatbot-iframe").src = "https://webchat.botframework.com/embed/smileMartBot-bot?s=" + json.key;
+}
+
+setIframeSrc()
