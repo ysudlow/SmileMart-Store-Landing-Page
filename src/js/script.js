90f312cfafb47f45d3511247347af33e2d56fac0
Gaetano <gaetanobarreca@gmail.com>
2023-10-22 19:21:28 -0700

    application json content type

--- src/js/script.js
+++ src/js/script.js
@@ -6,8 +6,12 @@ function customCloseForm() {
   document.getElementById("customMyForm").style.display = "none";
 }
 
-async function setIframeSrc(){
-  const response = await fetch('https://asa-yt7xyaocl7xlo-demo.azuremicroservices.io/get-chatbot-token');
+async function setIframeSrc() {
+  const response = await fetch('https://asa-yt7xyaocl7xlo-demo.azuremicroservices.io/get-chatbot-token',{
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
   const json = await response.json();
 
   debugger
